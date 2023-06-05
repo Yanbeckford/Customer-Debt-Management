0503661d3195daec3529d7509b5828090d33b5f2
Yannick <yanbeckford@gmail.com>
2023-06-05 22:28:11 +0100

    beginning of JS

---
+++ app.js
@@ -0,0 +1,16 @@
+function LoginValidation(){
+  let username = document.forms['lform']['username'].value
+  let passcode = document.forms['lform']['passcode'].value
+  if(username ==""){
+    alert('Enter username')
+
+  }else if(passcode ==""){
+    alert('Enter password')
+
+  }else if(username = 'admin' && passcode == 1234){
+    alert('Login successful')
+  }
+  }
+  
+  
+    
