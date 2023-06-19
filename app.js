b022a7ef66017655679f21b011ffa9a3c4276be0
Yannick <yanbeckford@gmail.com>
2023-06-19 11:07:40 +0100

    almost done

--- app.js
+++ app.js
@@ -14,8 +14,40 @@ function LoginValidation(){
   }
   }
   function customerForm(){
-    let custname = doc
+   let custName =  document.forms['custForm']['custName'].value;
+   let Loan = document.forms['custForm']['loan'].value;
+    let cust = localStorage.setItem('cust','custName').value;
+    let loan = localStorage.setItem('loan','Loan').value;
   }
-  
+  function selling(){
+    let seller = document.querySelector('#vendor').value;
+    if (seller = 'yannick'){
+      document.getElementById('debt').value = 100000;
+      document.getElementById('realamount').value = ((document.getElementById('saleprice').value) - (document.getElementById('debt').value));
+
+    }else if (seller = 'mirabel'){
+      document.getElementById('debt').value = 100000;
+      document.getElementById('realamount').value = ((document.getElementById('saleprice').value) - 100000);
+
+    }else if (seller = 'diana'){
+      document.getElementById('debt').value = 150000;
+      document.getElementById('realamount').value = ((document.getElementById('saleprice').value) - (document.getElementById('debt').value));
+
+    }else if (seller = 'stanwhite'){
+      document.getElementById('debt').value = 200000;
+      document.getElementById('realamount').value = ((document.getElementById('saleprice').value) - (document.getElementById('debt').value));
+
+    }
+  }
+  let yandebt = 100000;
+  let mirabeldebt = 100000;
+  let dianadebt = 150000;
+  let standebt = 200000;
+  let yansale = 500000 - yandebt;
+  let mirabelsale = 400000 - mirabeldebt;
+  let dianasale = 300000 - dianadebt;
+  let stansale = 200000 -standebt
+  document.getElementById('totaldebt').value = yandebt + mirabeldebt + dianadebt + standebt;
+  document.getElementById('balance').value = yansale + mirabelsale + dianasale + stansale; 
   
     
